f86347cdfbf1a75d97e1fafa55a91ed86e56fdd7
fjcx0000 <fjcx0000@gmail.com>
2016-09-03 18:21:47 +1000

    Add config files to github

---
+++ config/autoload/local.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Local Configuration Override
+ *
+ * This configuration override file is for overriding environment-specific and
+ * security-sensitive configuration information. Copy this file without the
+ * .dist extension at the end and populate values as needed.
+ *
+ * @NOTE: This file is ignored from Git by default with the .gitignore included
+ * in ZendSkeletonApplication. This is a good practice, as it prevents sensitive
+ * credentials from accidentally being committed into version control.
+ */
+
+return array(
+    'db' => array(
+        'driver'    => 'Pdo',
+        'username'  => 'root',
+        'password'  => '',
+        'dsn'       => 'mysql:dbname=gumtree_ads;host=localhost',
+        'driver_options'    => array(
+            \PDO::MYSQL_ATTR_INIT_COMMAND    => 'SET NAMES \'UTF8\''
+        ),
+    ),
+    'doctrine' => array(
+        'connection' => array(
+            'orm_default' => array(
+                'driverClass' => 'Doctrine\DBAL\Driver\PDOMySql\Driver',
+                'params' => array(
+                    'host'     => 'localhost',
+                    'port'     => '3306',
+                    'user'     => 'root',
+                    'password' => '',
+                    'dbname'   => 'gumtree_ads',
+                )
+            )
+        )
+    ),
+);
